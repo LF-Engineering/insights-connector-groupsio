dc4673df13d13fe74d2a14dc01aaef7c2e6d16a0
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-03-15 16:03:28 +0100

    DA-4849 (#6)
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- scripts/encrypt_example.sh
+++ scripts/encrypt_example.sh
@@ -7,6 +7,8 @@ export ENCRYPTION_BYTES="`cat ./secrets/ENCRYPTION_BYTES.dev.secret`"
 export ESURL="`cat ./secrets/ES_URL.prod.secret`"
 export STREAM=''
 export GROUPSIO_NO_INCREMENTAL=1
+# export GROUPSIO_ST=1
+# export GROUPSIO_NCPUS=1
 # curl -s -XPOST -H 'Content-Type: application/json' "${ESURL}/last-update-cache/_delete_by_query" -d'{"query":{"term":{"key.keyword":"Groups.io:hyperledger+fabric"}}}' | jq -rS '.' || exit 1
 ../insights-datasource-github/encrypt "`cat ./secrets/email.secret`" > ./secrets/email.encrypted.secret || exit 2
 ../insights-datasource-github/encrypt "`cat ./secrets/password.secret`" > ./secrets/password.encrypted.secret || exit 3
